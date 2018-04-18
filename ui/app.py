e08e800c84808fa59a194b2acc9accd77a853d8f
Ubuntu <ubuntu@ip-172-31-2-192.ap-south-1.compute.internal>
2018-04-18 02:14:00 +0000

    The model is now trained!

--- ui/app.py
+++ ui/app.py
@@ -23,6 +23,12 @@ Init seq2seq model
 '''
 #_________________________________________________________________
 import tensorflow as tf
+
+#import os,sys,inspect
+#currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+#parentdir = os.path.dirname(currentdir)
+#sys.path.insert(0,parentdir)
+
 import execute
 
 sess = tf.Session()
